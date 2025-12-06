bef37644699fff8588ddbe43930d4c610d3d7cbc
Sw33t23 <goncalopixadado@gmail.com>
2025-12-06 17:42:20 +0000

    firebase fix

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -4,17 +4,16 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAe32bC1a0YO ...", // keep your actual key
+  apiKey: "AIzaSyAe32bC1a0YOHg3Aokhgafq7E6wiy1_eK0",
   authDomain: "moviewatchlistapp-52230.firebaseapp.com",
   projectId: "moviewatchlistapp-52230",
   storageBucket: "moviewatchlistapp-52230.appspot.com",
   messagingSenderId: "737081148861",
-  appId: "1:737081148861:web:9444c4187b9029ecf97ed4",
+  appId: "1:737081148861:web:9444c4187b9029ecf97ed4"
 };
 
 const app = initializeApp(firebaseConfig);
 
-// React Native Auth initialization
 export const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage),
 });
